3c752277c6e596f94708300c8f3b6b76a914b7e1
Frederik Snedevind <fsn@mir-robots.com>
2020-12-11 14:31:09 +0100

    Continued on the SQL tutorial

--- pySQLiteTest/main.py
+++ pySQLiteTest/main.py
@@ -3,17 +3,49 @@ from sqlite3 import Error
 
 
 def create_connection(db_file):
-    """ create a database connection to a SQLite database """
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or Nonedatabase
+    """
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            return conn
 
+    return conn
+
+
+def create_project(conn, project):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    sql = ''' INSERT INTO projects(name,begin_date,end_date)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_task(conn, task):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
+              VALUES(?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+    return cur.lastrowid
 
 
 def create_table(conn, create_table_sql):
@@ -28,8 +60,103 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
+
+def update_task(conn, task):
+    """
+    update priority, begin_date, and end date of a task
+    :param conn:
+    :param task:
+    :return: project id
+    """
+    sql = ''' UPDATE tasks
+              SET priority = ? ,
+                  begin_date = ? ,
+                  end_date = ?
+              WHERE id = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+
+
+def delete_task(conn, id):
+    """
+    Delete a task by task id
+    :param conn:  Connection to the SQLite database
+    :param id: id of the task
+    :return:
+    """
+    sql = 'DELETE FROM tasks WHERE id=?'
+    cur = conn.cursor()
+    cur.execute(sql, (id,))
+    conn.commit()
+
+def delete_all_tasks(conn):
+    """
+    Delete all rows in the tasks table
+    :param conn: Connection to the SQLite database
+    :return:
+    """
+    sql = 'DELETE FROM tasks'
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+def delete_all_projects(conn):
+    """
+    Delete all rows in the projects table
+    :param conn: Connection to the SQLite database
+    :return:
+    """
+    sql = 'DELETE FROM projects'
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+def delete_all_rows(conn, row):
+    """
+    Delete all rows in the projects table
+    :param conn: Connection to the SQLite database
+    :return:
+    """
+    sql = 'DELETE FROM ' + row
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+
+def select_all_rows(conn, row):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM " + row)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+def select_task_by_priority(conn, priority):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks WHERE priority=?", (priority,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
 def main():
-    database = (r"./pythonsqlite.db")
+    database = "pythonsqlite.db"
 
     sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
                                         id integer PRIMARY KEY,
@@ -49,18 +176,33 @@ def main():
                                     FOREIGN KEY (project_id) REFERENCES projects (id)
                                 );"""
 
-    # create a database connection
+    # create a database connection, make sure tables exist
     conn = create_connection(database)
 
-    # create tables
-    if conn is not None:
-        # create projects table
+    with conn:
         create_table(conn, sql_create_projects_table)
-
-        # create tasks table
         create_table(conn, sql_create_tasks_table)
-    else:
-        print("Error! cannot create the database connection.")
+
+        delete_all_rows(conn, 'tasks')
+        delete_all_rows(conn, 'projects')       
+
+        # create a new project
+        project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30');
+        project_id = create_project(conn, project)
+
+        # tasks
+        task_1 = ('Analyze the requirements of the app', 1, 1, project_id, '2015-01-01', '2015-01-02')
+        task_2 = ('Confirm with user about the top requirements', 1, 1, project_id, '2015-01-03', '2015-01-05')
+
+        # create tasks
+        create_task(conn, task_1)
+        create_task(conn, task_2)
+
+        print("1. Query task by priority:")
+        select_task_by_priority(conn, 1)
+
+        print("\n2. Query all tasks")
+        select_all_rows(conn, 'tasks')
 
 
 if __name__ == '__main__':
